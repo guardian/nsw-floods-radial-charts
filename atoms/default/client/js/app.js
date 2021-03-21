23ad0d9c344e1be35fc0cff733054cc38b4613ff
nickjevershed <nick.evershed@gmail.com>
2021-03-21 22:13:03 +1100

    radial charts

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,2 +1,22 @@
-// if you want to import a module from shared/js then you can
-// just do e.g. import Scatter from "shared/js/scatter.js"
+import { radial } from "shared/js/radial";
+import * as d3 from "d3"
+
+Promise.all([
+	d3.csv(`<%= path %>/penrith.csv`)
+	])
+	.then((results) =>  {
+		radial(results[0], 'river', 'penrith', 200)
+		// var to=null
+		// var lastWidth = document.querySelector("#graphicContainer").getBoundingClientRect()
+		// window.addEventListener('resize', function() {
+		// 	var thisWidth = document.querySelector("#graphicContainer").getBoundingClientRect()
+		// 	if (lastWidth != thisWidth) {
+		// 		window.clearTimeout(to);
+		// 		to = window.setTimeout(function() {
+		// 			    init(results[0])
+		// 			}, 100)
+		// 	}
+		
+		// })
+
+	});
